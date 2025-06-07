3043ab54bed5581c9954a39a960c17964a118bf2
KrishGupta577 <krishna.gupta2022@sait.ac.in>
2025-06-07 19:04:09 +0530

    changs

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route, useNavigate } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import Dashboard from './pages/Dashboard/Dashboard';
 import ProtectedRoute from './ProtectedRoutes/ProtectedRoute';
-import { ToastContainer } from 'react-toastify'
+import { toast, ToastContainer } from 'react-toastify'
 import { useContext, useEffect } from 'react';
 import { StoreContext } from './context/StoreContext';
 import ExpenseForm from './components/AfterLogin/ExpenseForm/ExpenseForm';
@@ -13,6 +13,7 @@ function App() {
     const {colorTheme} = useContext(StoreContext)
 
     useEffect(() => {
+      toast.success("Note: The server may take up to 20 seconds to respond due to free hosting limitations. Thank you for your patience!")
       document.documentElement.setAttribute('data-theme', colorTheme);
     }, [colorTheme]);
 
